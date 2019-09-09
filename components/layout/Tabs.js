5d653b61323ab048fc119d39f3f331cbe3614feb
Some Guy <ozruen@gmail.com>
2019-09-09 23:49:15 +0800

    2.0.13 - Designed tab component

--- components/layout/Tabs.js
+++ components/layout/Tabs.js
@@ -1,11 +1,21 @@
 import React, { Component, useState, useEffect } from 'react';
-import { View, Image, Text, Animated } from 'react-native';
+import { View, Image, Text, Animated, TouchableOpacity } from 'react-native';
 
 export default class Tabs extends Component {
     render() {
         return (
-            <FadeInView style={{ height: '10%', backgroundColor: dominantColor,  justifyContent: 'center', alignItems: 'center' }}>
-                <Text style={{ fontFamily: 'Rye-Regular', color: accentColor, fontSize: 25 }}>Tabs Here.</Text>
+            <FadeInView style={{ height: '10%', backgroundColor: dominantColor, alignItems: 'center', flexDirection: 'row' }}>
+                <TouchableOpacity style={{ width: '33%' }}>
+                    <Text style={{ textAlign: 'center', fontFamily: 'Rye-Regular', color: accentColor, fontSize: 20 }}>Profile</Text>
+                </TouchableOpacity>
+                <View style={{ width: '0.5%', height: '100%', backgroundColor: accentColor }}></View>
+                <TouchableOpacity style={{ width: '33%' }}>
+                    <Text style={{ textAlign: 'center', fontFamily: 'Rye-Regular', color: accentColor, fontSize: 20 }}>New +</Text>
+                </TouchableOpacity>
+                <View style={{ width: '0.5%', height: '100%', backgroundColor: accentColor }}></View>
+                <TouchableOpacity style={{ width: '33%' }}>
+                    <Text style={{ textAlign: 'center', fontFamily: 'Rye-Regular', color: accentColor, fontSize: 20 }}>Jars</Text>
+                </TouchableOpacity>
             </FadeInView>
         );
     }
