1c932c6e14c53c48db4c09dfd3bd74cdd688c983
Some Guy <ozruen@gmail.com>
2019-09-10 15:33:45 +0800

    2.0.21 - Added Firebase

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { ImageBackground, Alert } from 'react-native';
+import firebase from 'react-native-firebase';
 
 import Header from './components/layout/Header';
 import Anonymous from './components/anonymous/Anonymous';
@@ -7,25 +8,37 @@ import Authorized from './components/authorized/Authorized';
 
 export default class HelloWorldApp extends Component {
   state = {
-    loggedIn: false,
-    header: 'Money Jar'
+    user: null
+  }
+  unsubscriber = null;
+
+  componentDidMount = () => {
+    this.unsubscriber = firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ user });
+    });
+  }
+
+  componentWillUnmount = () => {
+    if (this.unsubscriber) {
+      this.unsubscriber();
+    }
   }
 
   checkCredentials = (status, email, password) => {
     if (email == '' || password == '') {
       Alert.alert('Note', "Fields must not be empty!")
     } else {
-      this.setState({ loggedIn: status })
+      this.setState({ user: status })
     }
   }
 
   render() {
     return (
       <ImageBackground source={require('./assets/images/background.jpg')} style={{ flex: 1 }}>
-        <Header header={this.state.header} />
-        {this.state.loggedIn == true
-          ? <Authorized checkCredentials={this.checkCredentials} />
-          : <Anonymous checkCredentials={this.checkCredentials} />
+        <Header header={'Money Jar'} />
+        {!this.state.user
+          ? <Anonymous checkCredentials={this.checkCredentials} />
+          : <Authorized checkCredentials={this.checkCredentials} />
         }
       </ImageBackground>
     );
