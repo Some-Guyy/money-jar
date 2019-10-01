a3e45d6c2a041db39af62c229892873cc6f1a3ca
Some Guy <somenormaladdress@gmail.com>
2019-10-01 18:46:24 +0800

    Jar net worth not loaded text

--- components/authorized/Profile.js
+++ components/authorized/Profile.js
@@ -14,14 +14,20 @@ export default class Profile extends Component {
                 <View style={{ height: '76%' }}>
                     <Text style={[styles.bodyText, { fontSize: 24 }]}>Profile</Text>
                     <Text style={styles.bodyText}>Email: {this.props.user.email}</Text>
-                    <Text style={styles.bodyText}>Jar Net Worth: ${this.jarArray.reduce((accumulator, currentValue) => { return parseFloat(accumulator) + parseFloat(currentValue) }, 0).toFixed(2)}</Text>
+                    <Text style={styles.bodyText}>Jar Net Worth{
+                        isNaN(this.jarArray.reduce((accumulator, currentValue) => { return parseFloat(accumulator) + parseFloat(currentValue) }, 0).toFixed(2))
+                            ? " hasn't yet loaded, please go back to the Jars tab below to load your jars before coming back."
+                            : ": $" + this.jarArray.reduce((accumulator, currentValue) => { return parseFloat(accumulator) + parseFloat(currentValue) }, 0).toFixed(2)
+                    }</Text>
                     <TouchableOpacity onPress={this.props.resetPwd}><Text style={[styles.bodyText, { color: dominantColor, textDecorationLine: 'underline' }]}>Reset Password</Text></TouchableOpacity>
                 </View>
-                <TouchableNativeFeedback onPress={this.props.logout.bind(this)} background={TouchableNativeFeedback.Ripple()}>
-                    <View style={{ height: '9%', width: '30%', alignItems: 'center', backgroundColor: warnColor, padding: 10 }}>
-                        <Text style={[styles.bodyText, { color: accentColor }]}>Log out</Text>
-                    </View>
-                </TouchableNativeFeedback>
+                <View style={{ height: '9%', flexDirection: 'row' }}>
+                    <TouchableNativeFeedback onPress={this.props.logout.bind(this)} background={TouchableNativeFeedback.Ripple()}>
+                        <View style={{ width: '30%', alignItems: 'center', backgroundColor: warnColor, padding: 10 }}>
+                            <Text style={[styles.bodyText, { color: accentColor }]}>Log out</Text>
+                        </View>
+                    </TouchableNativeFeedback>
+                </View>
                 <View style={{ height: '10%' }}></View>
             </ViewFadeIn>
         )
